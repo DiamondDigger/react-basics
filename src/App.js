9bb94d4c13ff04ddc22041db6b04aaf4aa8a9587
naglec13 <naglec13@gmail.com>
2020-09-08 11:39:25 +0300

    - #2 - added component TodoList

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import React from 'react';
+import TodoList from "./Todo/TodoList";
 
 function App() {
   return (
-    <div className='wrapper'></div>
+    <div className='wrapper'>
+      <h1>radio React ive </h1>
+
+      <TodoList></TodoList>
+    </div>
   );
 }
 
