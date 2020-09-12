f3fd60a3e684b7fd8de9b297901bc1f34c113cfb
naglec13 <naglec13@gmail.com>
2020-09-13 00:35:59 +0300

    - #9 - added styles for Clock component

--- src/Clock.js
+++ src/Clock.js
@@ -1,6 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom"
 
+const styles = {
+    h2: {
+        margin: 'auto',
+        marginLeft: '5rem'
+    },
+    span: {
+        color: 'green'
+    }
+}
+
 export default class Clock extends React.Component{
     constructor(props) {
         super(props)
@@ -26,7 +36,8 @@ export default class Clock extends React.Component{
     render(){
         return (
             <div className='clockComponent'>
-                <h2>Now is {this.state.date.toLocaleTimeString()}</h2>
+                <span style={styles.span}> Do you know what? </span>
+                <h2 style={styles.h2}>Now is {this.state.date.toLocaleTimeString()}</h2>
             </div>
 
         )
