aefd073a766e04a3b07f2622a4386cbd644b0451
naglec13 <naglec13@gmail.com>
2020-09-18 17:27:44 +0300

    - #14 - added lazy loading

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, {useState, useEffect} from 'react';
 import TodoList from "./Todo/TodoList";
 import Context from "./context";
-import AddTodo from "./Todo/AddTodo";
 import Loader from "./Loader";
 
 function App() {
@@ -14,6 +13,12 @@ function App() {
 
     const [loading, setLoading] = useState(true)
 
+    const AddTodo = React.lazy(() => new Promise(resolve => {
+        setTimeout(() => {
+            resolve(import('./Todo/AddTodo'))
+        }, 2000)
+    }))
+
     useEffect(() => {
         fetch('https://jsonplaceholder.typicode.com/todos?_limit=5')
             .then(response => response.json())
@@ -22,7 +27,7 @@ function App() {
                         setData(data)
                         setLoading(false)
                     }
-                    , 5000)
+                    , 3000)
             )
     }, [])
 
@@ -57,7 +62,9 @@ function App() {
         <Context.Provider value={{removeTodoItem}}>
             <div className='wrapper'>
                 <h1>radio React ive </h1>
-                <AddTodo onCreate={addTodo}/>
+                <React.Suspense fallback={<p>Please wait, component is loading...</p>}>
+                    <AddTodo onCreate={addTodo}/>
+                </React.Suspense>
                 {data.length
                     ? <TodoList data={data} funcFromApp={onToggle}/>
                     : (loading
