6fbce9d7e9461761375e4a96c0e86bab76847c86
naglec13 <naglec13@gmail.com>
2020-09-19 00:28:45 +0300

    - #15 - added more logic in AddTodo component crossing with Modal component

--- src/Todo/AddTodo.js
+++ src/Todo/AddTodo.js
@@ -1,17 +1,23 @@
 import React, {useState} from "react";
 import PropType from 'prop-types'
 import Modal from "../Modal/Modal";
+import '../Modal/Modal.css'
 
 function useInputValue() {
     const [value, setValue] = useState( '')
-
+    const [info, setInfo] = useState('Some description should be here')
     return {
-        bind: {
+        bindValue: {
             value,
-            onChange: event => setValue(event.target.value)
+            onChange: (event) => setValue(event.target.value)
+        },
+        bindInfo: {
+            info,
+            onChange: (event) => setInfo(event.target.value)
         },
         clear: () => setValue(''),
-        value: () => value
+        value: () => value,
+        info: () => info
     }
 }
 
@@ -32,9 +38,28 @@ function AddTodo({onCreate}) {
     return (
         <div>
             <form className='inputForm' onSubmit={submitHandler}>
-                <input type="text" {...input.bind} />
+                <input type="text" {...input.bindValue} />
                 <button type='submit'>Add Todo</button>
             </form>
+
+            <React.Fragment>
+                <button onClick={this.setState({turnOn: true})}>Open modal</button>
+
+                {this.state.turnOn &&
+                (<div className='modal'>
+                    <div className='modal-body'>
+                        <h2>{input.value}</h2>
+                        <input type="text" {...input.bindValue}/>
+
+                        <p>{input.info}</p>
+                        <input type="text" {...input.bindInfo} />
+                        <button style={{display: 'block', marginTop: '1rem'}}
+                                onClick={() => this.setState({turnOn: false})} >Close modal</button>
+                    </div>
+                </div>)
+                }
+            </React.Fragment>
+
         </div>
     )
 }
